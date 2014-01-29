5f92d63f2e417bc03e4e4b259b6a002b4fe77ffd
AreejOmBas <aob@aobs-Mac.local>
2014-01-28 16:11:30 -0800

    half of Bank class

--- KingsNThings/Bank.h
+++ KingsNThings/Bank.h
@@ -9,5 +9,32 @@
 #import <Foundation/Foundation.h>
 
 @interface Bank : NSObject
+{
+    /*20 of 1golds, 2golds,5golds,10golds
+     10 of fifteengold and twentyGold
+     */
+    
+    NSInteger oneGold;
+    NSInteger twoGold;
+    NSInteger fiveGold;
+    NSInteger tenGold;
+    NSInteger fifteenGold;
+    NSInteger twentyGold;
+    
+    UIButton* oneGoldB,*twoGoldB,*fiveGoldB,*tenGoldB,*fifteenGoldB,*twentyGoldB;
+    
+    
+    
+    
+}
+
+@property NSInteger oneGold;
+@property NSInteger twoGold;
+@property NSInteger fiveGold;
+@property NSInteger tenGold;
+@property NSInteger fifteenGold;
+@property NSInteger twentyGold;
+
+- (NSInteger) updateBalance;
 
 @end
